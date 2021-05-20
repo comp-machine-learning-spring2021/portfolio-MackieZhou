0e6f91d6915599645c939e708b0029813720faf6
MackieZhou <mzhou@smith.edu>
2021-05-21 02:38:38 +0800

    update coding 3 unit tests

--- coding_3_supervised/test_coding_3.py
+++ coding_3_supervised/test_coding_3.py
@@ -6,6 +6,8 @@ import numpy as np
 import coding_3
 
 data = np.genfromtxt("coding_3_supervised\hw6data.csv", delimiter=',')
+features = data[:, :2]
+targets = data[:, 2]
 
 
 def test_split_val_data_type():
@@ -14,3 +16,8 @@ def test_split_val_data_type():
 
 def test_split_val_data_shape():
     assert len(coding_3.split_val_data(data, 10)) == 2
+
+
+def test_random_forest_cross_val_type():
+    out = coding_3.random_forest_cross_val(10, features, targets, 10, 3, 2)
+    assert isinstance(out, float)
